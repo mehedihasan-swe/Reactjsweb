f7073eb02043a39ac039df5fe8b48a60cdfc565a
mehedihasan-swe <mh121499@gmail.com>
2025-01-29 16:48:37 +0600

    [Feat][Style] Added global css for override default theme

--- admindashboard/src/App.js
+++ admindashboard/src/App.js
@@ -1,10 +1,28 @@
 
-import { CssBaseline} from '@mui/material';
+import { createTheme, CssBaseline, ThemeProvider} from '@mui/material';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import { makeStyles } from '@mui/styles';
 
+
+// you can override default theme by this create theme 
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#333996",
+      light: '#3c44b126'
+    },
+    secondary: {
+      main: "#f83245",
+      light: '#f8324526'
+    },
+    background: {
+      default: "#f4f5fd"
+    },
+  }
+})
 
 const ustyle=makeStyles({
   appMain:{
@@ -15,11 +33,14 @@ const ustyle=makeStyles({
 function App() {
   const classes=ustyle()
   return (
+    <ThemeProvider theme={theme}>
+     <Sidebar></Sidebar>
     <div className={classes.appMain}>
-      <Sidebar></Sidebar>
-      <Header></Header>
-      <CssBaseline/>
+      <Header/>
     </div>
+    <CssBaseline/>
+    </ThemeProvider>
+   
     
   );
 }
--- admindashboard/src/components/Header.js
+++ admindashboard/src/components/Header.js
@@ -8,10 +8,38 @@ import SearchIcon from '@mui/icons-material/Search';
 import { makeStyles } from '@mui/styles';
 
 
+
+
+
 const useStyles=makeStyles({
   root:{
     backgroundColor: '#ffffff !important',
-  }
+  },
+  searchInput:{
+    opacity:'0.6',
+    padding:'0 8px',
+    fontSize:'0.8rem',
+    
+    '&:hover':{
+    backgroundColor:'#f2f2f2'
+    },
+    '& .MuiSvgIcon-root': {
+      marginRight: '8px',
+  
+    },
+  },
+
+  btnroot:{
+    backgroundColor:'#d3d3d3 !important'
+  },
+
+  btnlabel: {
+    '& .MuiSvgIcon-root': {
+      marginRight: '8px',
+      color: 'blue !important', // Set the desired color
+      fontSize: '24px', // Optional: Adjust the size of the icon if needed
+    },
+  },
 })
 
 
@@ -27,25 +55,21 @@ const classes=useStyles();
           <Box sx={{ flexGrow: 1, px: 2 }}>
             <Stack direction='row' justifyContent='space-between' alignItems='center'>
               <Box>
-                <InputBase
-                placeholder='Search holder'
-                startAdornment={<SearchIcon fontSize='small'/>}
-                />
+                <InputBase placeholder='Search holder' className={classes.searchInput} startAdornment={<SearchIcon fontSize='small'/>}/>
               </Box>
               <Box sx={{ display: 'flex', gap: '15px' }}>
-                <IconButton>
+                <IconButton classes={{ root: classes.btnroot, label: classes.btnlabel }}>
                   <MailIcon />
                 </IconButton>
 
-                <IconButton>
+                <IconButton color='primary'> 
                   <LogoutIcon />
                 </IconButton>
 
-                <IconButton>
+                <IconButton className={classes.btnlabel}>
                   <FaceIcon />
                 </IconButton>
               </Box>
-
             </Stack>
           </Box>
         </Toolbar>
