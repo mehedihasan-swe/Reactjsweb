53204af202d69ae6c1c542eb2c0f4b23ba3e790b
mehedihasan-swe <mh121499@gmail.com>
2025-02-03 19:03:50 +0600

    [Style][Feat] Added Onchange Event and functionality

--- admindashboard/src/pages/Employees/Employee.js
+++ admindashboard/src/pages/Employees/Employee.js
@@ -10,7 +10,7 @@ const useStyles=makeStyles(theme=>({
     margin:theme.spacing(2),
 
     '& .MuiFormControl-root':{
-      width:"99%"
+      width:"100%"
     },
   }
 }))
@@ -31,7 +31,15 @@ const initaialValue={
 export default function Employee() {
   const classes=useStyles()
  
-  const [value]=useState(initaialValue)
+  const [values, setValue]=useState(initaialValue)
+
+  const handleInput=e=>{
+    const {name, value}=e.target
+     setValue({
+      ...values,
+      [name]:value
+     })
+  }
 
   return (
     <>
@@ -44,21 +52,30 @@ export default function Employee() {
         <Box sx={{ width: '100%' }}>
           <Grid2 container rowSpacing={1} columnSpacing={3}>
             <Grid2 size={7}>
-              <TextField
-              variant='outlined'
-              name="fullName"
-              label="Full Name"
-              value={value.fullName}
-              />
-              
+              <Grid2 container rowSpacing={1} columnSpacing={3}>
+              <Grid2 size={{xs:12, md:10}}>
+                <TextField
+                variant='outlined'
+                name="fullName"
+                label="Full Name"
+                value={values.fullName}
+                onChange={handleInput}
+                />
+                
+              </Grid2>
+              <Grid2 size={{xs:12, md:10}}>
+                <TextField
+                variant='outlined'
+                name='email'
+                label="Email"
+                value={values.email}
+                onChange={handleInput}
+                />
+              </Grid2>
+              </Grid2>
             </Grid2>
-            <Grid2 size={7}>
-              <TextField
-              variant='outlined'
-              name='email'
-              label="Email"
-              value={value.email}
-              />
+            <Grid2 size={5}>
+
             </Grid2>
           </Grid2>
         </Box>
